fe1e1ba572a730e5d6c372529ef6d626f5d6e405
IKlimuk <i.klimuk2022@gmail.com>
2023-01-18 18:12:18 +0200

    firs test

--- src/com/ua/robotd/homework2/HelloWorld.java
+++ src/com/ua/robotd/homework2/HelloWorld.java
@@ -1,7 +1,13 @@
 package com.ua.robotd.homework2;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter: ");
+
+        int frInt = scanner.nextInt();
+        System.out.println("Exit: " + frInt);
     }
 }
