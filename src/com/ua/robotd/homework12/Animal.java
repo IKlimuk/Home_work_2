07d5bd7c7bf96aca7a4aa95c3c9bad73cfe31253
IKlimuk <i.klimuk2022@gmail.com>
2023-02-21 18:11:10 +0200

    lesson_12_classes

---
+++ src/com/ua/robotd/homework12/Animal.java
@@ -0,0 +1,16 @@
+package com.ua.robotd.homework12;
+
+public class Animal {
+
+    static {
+        System.out.println("1");
+    }
+
+    {
+        System.out.println("3");
+    }
+
+    public Animal() {
+        System.out.println("4");
+    }
+}
---
+++ src/com/ua/robotd/homework12/Main.java
@@ -0,0 +1,7 @@
+package com.ua.robotd.homework12;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat ron = new Cat();
+    }
+}
